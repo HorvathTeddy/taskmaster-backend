2d244e1db0cecb7c28a8a3727c855bc44864a7f4
Teddy Horvath <teddyhorvath@gmail.com>
2022-11-03 09:30:08 -0700

    implemented root server route

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import Cors from 'cors'
+const express = require('express')
+//import mongoose from 'mongoose'
+//import Cors from 'cors'
+const path = require('path')
 
 // App config
 const app = express()
 const port = process.env.PORT || 8001
 
-// API Endpoint
-app.get("/", (req, res) => res.status(200).send("WE ON"))
+// API Endpoints
+app.use('/', express.static(path.join(__dirname, '/public')))
+app.use('/', require('./routes/root'))
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) res.sendFile(path.join(__dirname, 'views', '404.html'))
+    else if (req.accepts('json')) res.json({ message: '404 Not Found' })
+    else res.type('txt').send('404 Not Found')
+})
 
 // Listenter
 app.listen(port, () => console.log(`listening on port: ${port}`))
